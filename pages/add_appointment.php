8f4bbdf9580ffba3658ffcf8a37c40291ecf4739
Zaki Omer <90000390+ZakiOmer22@users.noreply.github.com>
2025-06-03 18:44:53 +0300

    Removed Local Tailwindcss

--- pages/add_appointment.php
+++ pages/add_appointment.php
@@ -4,13 +4,6 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <title>Add Appointment</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-
 <body class="bg-gray-100 font-sans">
     <div class="ml-64">
         <main class="pt-20 px-6">
